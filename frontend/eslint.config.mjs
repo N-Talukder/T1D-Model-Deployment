d49972383f3e623ed1a3c3e6218132bd96e63849
N-Talukder <nirzortalukder@gmail.com>
2026-01-04 13:00:09 +0200

    add eslint and prettier and define rules for frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,17 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierRecommended from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  /* Add prettier */
+  {
+    plugins: { prettier: prettierPlugin },
+    extends: [prettierRecommended],
+    rules: {
+      // Report Prettier issues as ESLint errors
+      'prettier/prettier': ['error'],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
