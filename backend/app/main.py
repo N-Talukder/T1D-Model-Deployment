37794ae4eb707393e30a9a34a8b7f679c84b5b26
N-Talukder <nirzortalukder@gmail.com>
2026-01-06 22:35:19 +0200

    enable cors in the backend to serve requests from all, when running (only for the development time, this will be changed before production built is made)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware for serving requests from different origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
